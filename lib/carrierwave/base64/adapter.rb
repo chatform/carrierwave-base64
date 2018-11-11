435d15231de83f1d84b6b3f634eb8f5b05016404
Ian Graham <imartingraham@gmail.com>
2018-11-10 16:42:42 -0800

    added mount_base64_uploaders

--- lib/carrierwave/base64/adapter.rb
+++ lib/carrierwave/base64/adapter.rb
@@ -28,6 +28,18 @@ module Carrierwave
           self, attribute, options
         )
       end
+
+      def mount_base64_uploaders(attribute, uploader_class, options = {})
+        mount_uploaders attribute, uploader_class, options
+        options[:file_name] ||= proc { attribute }
+
+        Carrierwave::Base64::MountingHelper.check_for_deprecations(options)
+
+        Carrierwave::Base64::MountingHelper.define_writer(
+          self, attribute, options
+        )
+      end
+
     end
   end
 end
